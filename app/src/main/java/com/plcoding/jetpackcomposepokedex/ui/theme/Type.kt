5b8893bd872dcd872569b0273ddbf933e1a7154a
Philipp <mike.litoris@gmail.com>
2021-04-23 14:40:37 +0200

    Initial setup

--- app/src/main/java/com/plcoding/jetpackcomposepokedex/ui/theme/Type.kt
+++ app/src/main/java/com/plcoding/jetpackcomposepokedex/ui/theme/Type.kt
@@ -2,17 +2,31 @@ package com.plcoding.jetpackcomposepokedex.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.plcoding.jetpackcomposepokedex.R
+
+val Roboto = FontFamily(
+    Font(R.font.roboto_light, FontWeight.Light),
+    Font(R.font.roboto_regular, FontWeight.Normal),
+    Font(R.font.roboto_medium, FontWeight.Medium),
+    Font(R.font.roboto_bold, FontWeight.Bold),
+)
+
+val RobotoCondensed = FontFamily(
+    Font(R.font.roboto_condensed_light, FontWeight.Light),
+    Font(R.font.roboto_condensed_regular, FontWeight.Normal),
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     body1 = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = Roboto,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp
-    )
+    ),
     /* Other default text styles to override
     button = TextStyle(
         fontFamily = FontFamily.Default,
