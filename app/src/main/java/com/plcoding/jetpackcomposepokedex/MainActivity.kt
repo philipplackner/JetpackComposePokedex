42c838158a2f8f84c1864bbfddd88f2dc51e2ec8
Blackblind <shantanujaiswaloyu@gmail.com>
2024-02-20 13:25:16 +0530

    adding material3 and Koylin DSL

--- app/src/main/java/com/plcoding/jetpackcomposepokedex/MainActivity.kt
+++ app/src/main/java/com/plcoding/jetpackcomposepokedex/MainActivity.kt
@@ -3,9 +3,6 @@ package com.plcoding.jetpackcomposepokedex
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.plcoding.jetpackcomposepokedex.ui.theme.JetpackComposePokedexTheme
